a7f441d4edf4fe9fa13206970ab355e2c2636fae
mendozaro25 <jmendozaro73@gmail.com>
2022-11-07 16:23:27 -0500

    Update 7/11/2022 16:23:00

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { WelcomeService } from './welcome/welcome.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(
